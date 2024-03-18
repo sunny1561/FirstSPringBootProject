40578324a4f580657d9fb69391f8a6d809244efb
sunnysoni <sunnykumar@iitbhilai.ac.in>
2024-03-19 02:32:59 +0530

    Initial commit

---
+++ src/test/java/com/sunny/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
